6a471858c6e086f93f14c7dd62b95b2ffe61da47
lazy <98337661+lazy-top@users.noreply.github.com>
2026-03-26 12:10:19 +0800

    Create Struct.py

---
+++ Struct.py
@@ -0,0 +1,14 @@
+from isaacsim import SimulationApp
+
+# 配置isaacsim应用参数
+config = {
+    "headless": False,
+    # "hide_ui": True,
+
+    "width": 1280,
+    "height": 720,
+}
+# 启动isaacsim应用
+simulation_app = SimulationApp(config)
+
+# 
